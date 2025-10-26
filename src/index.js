4cd0709dff996b29c1dfc30a79f055664613e39b
Gurjinder <dev.gurjinder@gmail.com>
2025-10-26 13:11:31 +0000

    clean up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 /**
  * @param {...*} args - Required*
  * @example log("B", varA, varB...) OR log(varA...)
@@ -9,7 +8,6 @@
 */
 
 function log(...args) {
-
   if (args?.length == 0) {
     throw new Error("Empty arguments in function call!");
   }
@@ -17,20 +15,14 @@ function log(...args) {
   const logColor = returnColor(args[0] ?? "Y");
   for (const arg of args) {
     const type = Object.prototype.toString.call(arg).split(" ")[1];
-    // console.log(logColor)
-    // console.log(returnColor(arg), logColor)
     if (matchesAnyColorSymbol(arg)) {
       continue;
     }
-
-    // console.log(type);
     if (type === "Object]") {
       console.log(`%c ${JSON.stringify(arg)}`, ` color: ${logColor} `);
     } else if (type === "Array]") {
       for (const val of arg) {
-        // console.log(val)
         const dataType = Object.prototype.toString.call(val).split(" ")[1];
-        // console.log(dataType);
         if (dataType === "Object]") {
           console.log(`%c ${JSON.stringify(val)}`, ` color: ${logColor} `);
         } else {
@@ -41,22 +33,19 @@ function log(...args) {
       console.log(`%c ${arg}`, ` color: ${logColor} `);
     }
   }
-
 }
 
 /**
- * 
+ *
  * @param {*} title  - Required* for the title
  * @param  {...any} args -  first argument should be a color symbol string ("Y", "R", "G", "O", "B", "P", "C", "V") followed by everything you want to log. Default will be Yellow color.
  * @example headlog("My heading", "B", varA, varB...) OR headlog("My heading", varA, varB) - yellow default
  * @default yellow
  * @description headlog functions is to be used when you want a title on the first line of console followed by all things you want to log. The styling only applies
- * to the title in this case and other arguments will be logged as standard console log style.  
+ * to the title in this case and other arguments will be logged as standard console log style.
  */
 function headlog(title, ...args) {
-  // console.log(args)
-  // console.log(title, args)
-  if (!!title && typeof(title)==='string') {
+  if (!!title && typeof title === "string") {
     const color = returnColor(args[0] ?? "Y");
     console.log(
       `%c${title}`,
@@ -65,44 +54,15 @@ function headlog(title, ...args) {
 
     for (const arg of args) {
       if (matchesAnyColorSymbol(arg)) {
-        continue
+        continue;
       } else {
-        console.log(arg)
+        console.log(arg);
       }
-      
     }
   } else {
-    throw new Error(
-      "Title is required as first argument!"
-    );
+    throw new Error("Title is required as first argument!");
   }
 }
-
-
-const a = 10;
-const b = 20;
-const c = {
-  name: "gssds",
-  age: 20,
-  address: {
-    street: "asdasd",
-  },
-  sex: "Male",
-};
-
-const d = [
-  {
-    name: "asdasd",
-  },
-  {
-    name: "gurjinder",
-  },
-];
-const e = [1, 2, 4];
-const name = "My heading"
-
-// log(a, b, d, c, e,);
-// headlog(name, "G", a, d,c)
 
 function returnColor(color) {
   switch (color) {
@@ -127,11 +87,18 @@ function returnColor(color) {
   }
 }
 
-function matchesAnyColorSymbol (color) {
-  if( color === "Y" || color === "R" || color === "G" || color === "B" || color === "C"
-    || color === "V" || color === "O" || color === "P"
+function matchesAnyColorSymbol(color) {
+  if (
+    color === "Y" ||
+    color === "R" ||
+    color === "G" ||
+    color === "B" ||
+    color === "C" ||
+    color === "V" ||
+    color === "O" ||
+    color === "P"
   ) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
